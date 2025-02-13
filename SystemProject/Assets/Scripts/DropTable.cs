d64112ee0d427e491476b7645ae9d87f5e48a665
shinjiyong <shinjiyong0904@gmail.com>
2025-02-13 11:12:17 +0900

    드랍 테이블을 보유한 적 만들기

---
+++ SystemProject/Assets/Scripts/DropTable.cs
@@ -0,0 +1,9 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="DropTable", menuName ="DropTable/drop table", order =0)]
+public class DropTable : ScriptableObject
+{
+    public List<GameObject> drop_table;
+}
--- SystemProject/Assets/Scripts/Enemy.cs
+++ SystemProject/Assets/Scripts/Enemy.cs
@@ -1,16 +1,28 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    //몬스터의 드랍 테이블
+    public DropTable DropTable;
+
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Dead();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Dead()
     {
-        
+        //드랍 테이블 내 아이템을 랜덤으로 선택
+        GameObject dropItemPrefab = 
+            DropTable.drop_table[Random.Range(0,DropTable.drop_table.Count)];
+
+        Instantiate(dropItemPrefab, transform.position, Quaternion.identity);
+
+        Destroy(gameObject);
     }
 }
