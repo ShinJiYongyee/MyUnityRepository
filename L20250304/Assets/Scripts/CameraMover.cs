55e36f05a7e7b352ede7ea73319303965896931f
shinjiyong <shinjiyong0904@gmail.com>
2025-03-05 17:47:29 +0900

    3인칭 캐릭터 시점

--- L20250304/Assets/Scripts/CameraMover.cs
+++ L20250304/Assets/Scripts/CameraMover.cs
@@ -1,11 +1,15 @@
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class CameraMover : MonoBehaviour
 {
     Transform Player;
     float positionDelay = 2.0f;
     float rotationDelay = 2.0f;
+
+    Vector3 currentVelocity;
+    float smoothTime = 0.3f;
     void Awake()
     {
         Player = GameObject.FindGameObjectWithTag("Player").transform;
@@ -14,7 +18,9 @@ public class CameraMover : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = Vector3.Lerp(transform.position, Player.position, positionDelay*Time.deltaTime);
+        //transform.position = Vector3.Lerp(transform.position, Player.position, positionDelay*Time.deltaTime);
+        transform.position = Vector3.SmoothDamp(transform.position, Player.position, ref currentVelocity, smoothTime);
         transform.rotation = Quaternion.Lerp(transform.rotation, Player.rotation, rotationDelay*Time.deltaTime);
+        
     }
 }
--- L20250304/Assets/Scripts/PlayerController.cs
+++ L20250304/Assets/Scripts/PlayerController.cs
@@ -1,33 +1,37 @@
 using UnityEngine;
 
+
+//입력이나 다른 이벤트를 처리한다.
+//현재 게임 오브젝트에 다른 컴포넌트에 명령을 내린다.
+//사용자 컴포넌트는 한가지 일만 해야 된다.
 public class PlayerController : MonoBehaviour
 {
-    float moveSpeed = 3.0f;
-    float rollSpeed = 60.0f;
-    float pitchSpeed = 60.0f;
-    float yawSpeed = 60.0f;
+    MeshRenderer meshRenderer;
 
+    float moveSpeed = 3.0f;
+    float rotationSpeed = 60.0f;
+
+
+    void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+
+    // Update is called once per frame
     void Update()
     {
-        float pitch = Input.GetAxisRaw("Vertical");
-        float roll = Input.GetAxisRaw("Horizontal");
-        float yaw = 0;
+        //velocity => vector 크기랑 방향
+        //s = v * t => speed * direction * time 
 
-        if (Input.GetKey(KeyCode.E))
-        {
-            yaw++;
-        }
-        if (Input.GetKey(KeyCode.Q))
-        {
-            yaw--;
-        }
+        float v = Input.GetAxisRaw("Vertical");
+        float h = Input.GetAxisRaw("Horizontal");
 
-        // 전진 이동
-        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
 
-        // 올바른 회전 방식 적용
-        transform.Rotate(Vector3.right * pitch * Time.deltaTime * pitchSpeed, Space.Self); // Pitch
-        transform.Rotate(Vector3.up * yaw * Time.deltaTime * yawSpeed, Space.Self);       // Yaw
-        transform.Rotate(Vector3.forward * -roll * Time.deltaTime * rollSpeed, Space.Self); // Roll
+        //로컬좌표계, 월드 좌표계
+        //세상의 위방향
+        //transform.position += transform.up * v * Time.deltaTime * moveSpeed;
+        transform.Translate(Vector3.forward * -v * Time.deltaTime * moveSpeed);
+        transform.Rotate(transform.up * -h * Time.deltaTime * rotationSpeed);
     }
 }
